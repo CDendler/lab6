ae27f709ccf303f9bbb0de9b86e4dcfab457b73d
CDendler <144456701+CDendler@users.noreply.github.com>
2023-10-29 21:23:46 -0400

    Add files via upload

--- solution.py
+++ solution.py
@@ -13,28 +13,58 @@ system = [central_body, planet1, planet2]
 
 def calculate_distance(body1, body2):
     """Returns the distance between two bodies"""
-    pass
+    distance = math.sqrt((body1[1][0] - body2[1][0])^2 + (body1[1][3]-body2[1][3])^2)
+    abs_between = abs(distance)
+    return abs(distance)
+
 
 def calculate_force(body1, body2):
-    """Returns the force exerted on body1 by body2, in 2 dimensions as a tuple"""
-    pass
+    force1 = (G * ((body1[0]*body2[0])/calculate_distance()^2))
+    distance = math.sqrt((body1[1][0] - body2[1][0])^2 + (body1[1][3]-body2[1][3])^2)
+    abs_between = abs(distance)
+    rx = force1 * ((body2[1][0] - body1[1][0]))
+    ry = force1 * ((body2[1][1] - body1[1][1]))
+    forcex = force1 * rx / abs_between
+    forcey = force1 * ry / abs_between
+    return (forcex, forcey)
 
 def calculate_net_force_on(body, system):
     """Returns the net force exerted on a body by all other bodies in the system, in 2 dimensions as a tuple"""
-    pass
+    totalforcex = 0
+    totalforcey = 0
+    for not_home_planet in system:
+        if body != not_home_planet:
+            forcex , forcey = calculate_force(body, not_home_planet)
+            totalforcex += forcex
+            totalforcey += forcey
+    return totalforcex, totalforcey
+
 
 def calculate_acceleration(body, system):
     """Returns the acceleration of a body due to the net force exerted on it by all other bodies in the system, in 2 dimensions as a tuple"""
-    pass
+    total_force = calculate_net_force_on(body, system)
+    mass = body[0]
+    acceleration = (total_force[0] /mass, total_force[1] / mass)
+    return acceleration 
 
 def update_velocity(system, dt):
     """Updates the velocities of all bodies in the system, given a time step dt"""
-    pass
+    newsystem_velocity = []
+    for body in system:
+        mass, (x,y), (vx, vy) = body
+        totalforcex, totalforcey = calculate_net_force_on(body, system)
+        accelerationx, accelerationy = totalforcex / mass, totalforcey / mass
+        vx_new = vx + accelerationx *dt
+        vy_new = vy + accelerationy *dt
+        new_body = (mass, (x, y), vx_new, vy_new)
+        newsystem_velocity.append(new_body)
+    return newsystem_velocity 
    
 
 def update(system, dt):
     """Update the positions of all bodies in the system, given a time step dt"""
-    pass
+    newsystem_velocity = update_velocity(system, dt)
+    
 
 def simulate(system, dt, num_steps):
     """Simulates the motion of a system of bodies for a given number of time steps"""
